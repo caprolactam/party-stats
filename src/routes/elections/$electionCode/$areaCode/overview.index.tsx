aa088b5223605fec8be67011a81e2d25cfacdaa1
caprolactam <caprolactam113@gmail.com>
2025-04-28 14:38:44 +0900

    ui: changes overview layout
    chore: fix component key
    
    chore: change react key for consistent

--- src/routes/elections/$electionCode/$areaCode/overview.index.tsx
+++ src/routes/elections/$electionCode/$areaCode/overview.index.tsx
@@ -3,12 +3,18 @@ import {
   useLoaderData,
   notFound,
 } from '@tanstack/react-router'
+import { useSessionStorage } from 'usehooks-ts'
 import { type GetElectionOverview } from '#api/schema.ts'
+import { Icon, type IconName } from '#src/components/parts/icon.tsx'
+import {
+  ToggleGroup,
+  ToggleGroupItem,
+} from '#src/components/parts/toggle-group.tsx'
 import {
   NotFoundComponent,
   ErrorBoundary,
 } from '#src/components/templates/misc.tsx'
-import { countFormatter, useAreaInfo } from '#src/utils/misc.ts'
+import { strictEntries, countFormatter, useAreaInfo } from '#src/utils/misc.ts'
 import { VoteResult } from '#src/utils/vote-result.tsx'
 
 async function getElectionOverview({
@@ -53,6 +59,18 @@ export const Route = createFileRoute(
   gcTime: 0,
 })
 
+const viewOptions = {
+  card: {
+    label: 'カード',
+    icon: 'grid-view',
+  },
+  list: {
+    label: 'リスト',
+    icon: 'view-list',
+  },
+} satisfies Record<string, { label: string; icon: IconName }>
+type ViewOption = keyof typeof viewOptions
+
 function RouteComponent() {
   const { parties, totalCount } = Route.useLoaderData()
   const {
@@ -61,9 +79,11 @@ function RouteComponent() {
     from: '/elections/$electionCode',
   })
   const areaInfo = useAreaInfo()
-  const electionCode = Route.useParams().electionCode
+  const { electionCode, areaCode } = Route.useParams()
   const title = `${areaInfo.label}の選挙結果`
   const description = `${electionName}における${title}`
+
+  const [view, setView] = useSessionStorage<ViewOption>('overview-view', 'card')
 
   return (
     <>
@@ -73,40 +93,80 @@ function RouteComponent() {
         content={description}
       />
       <div className='mt-(--space-base) grid gap-(--space-base)'>
-        <h2 className='text-xl font-bold'>{title}</h2>
         <div>
-          <div className='mb-2 flex justify-between text-base font-normal'>
+          <h2 className='text-xl font-bold'>{title}</h2>
+          <div className='mt-(--space-xs) flex justify-between'>
             <div>{`総得票数 : ${countFormatter.format(totalCount)}`}</div>
             <div>【単位 : 票】</div>
           </div>
-          <VoteResult
-            areaCode={areaInfo.areaCode}
-            electionCode={electionCode}
-            className='grid grid-cols-[repeat(auto-fit,_minmax(18rem,_1fr))] items-start gap-4'
-          >
-            {parties.map((party) => {
-              const increaseCount = party.prevCount
-                ? party.count - party.prevCount
-                : null
-              const increaseRate = party.prevRate
-                ? party.rate - party.prevRate
-                : null
-
-              return (
-                <VoteResult.Item
-                  key={`${party.code}-${areaInfo.areaCode ?? 'national'}-${electionCode}`}
-                  partyName={party.name}
-                  partyCode={party.code}
-                  partyColor={party.color}
-                  count={party.count}
-                  rate={party.rate}
-                  increaseCount={increaseCount}
-                  increaseRate={increaseRate}
-                />
-              )
-            })}
-          </VoteResult>
         </div>
+        <div>
+          <ToggleGroup
+            className='w-full sm:w-fit'
+            type='single'
+            value={view}
+            onValueChange={(value) => {
+              if (value === 'card' || value === 'list') {
+                setView(value)
+              }
+            }}
+          >
+            {strictEntries(viewOptions).map(([key, { label, icon }]) => (
+              <ToggleGroupItem
+                key={key}
+                value={key}
+              >
+                <Icon
+                  name={view === key ? 'check' : icon}
+                  size={18}
+                />
+                {label}
+              </ToggleGroupItem>
+            ))}
+          </ToggleGroup>
+        </div>
+
+        <VoteResult
+          areaCode={areaCode}
+          electionCode={electionCode}
+          className={
+            view === 'card'
+              ? 'grid grid-cols-[repeat(auto-fit,_minmax(18rem,_1fr))] items-start gap-4'
+              : 'grid'
+          }
+        >
+          {parties.map((party) => {
+            const key = `${electionCode}-${areaCode}-${party.code}`
+            const increaseCount = party.prevCount
+              ? party.count - party.prevCount
+              : null
+            const increaseRate = party.prevRate
+              ? party.rate - party.prevRate
+              : null
+
+            return view === 'card' ? (
+              <VoteResult.Card
+                key={`${key}-card`}
+                partyName={party.name}
+                partyCode={party.code}
+                partyColor={party.color}
+                count={party.count}
+                rate={party.rate}
+                increaseCount={increaseCount}
+                increaseRate={increaseRate}
+              />
+            ) : (
+              <VoteResult.ListItem
+                key={`${key}-list-item`}
+                partyName={party.name}
+                partyCode={party.code}
+                partyColor={party.color}
+                count={party.count}
+                rate={party.rate}
+              />
+            )
+          })}
+        </VoteResult>
       </div>
     </>
   )
