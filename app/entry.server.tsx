d37228933b291c0bd705a3b240e5c70066392ae7
caprolactam <caprolactam113@gmail.com>
2025-07-31 04:18:09 +0900

    feat: フォーマッターとリンターの更新

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,10 +1,7 @@
 import { isbot } from 'isbot'
 import { renderToReadableStream } from 'react-dom/server'
-import {
-  type AppLoadContext,
-  type EntryContext,
-  ServerRouter,
-} from 'react-router'
+import { ServerRouter } from 'react-router'
+import type { AppLoadContext, EntryContext } from 'react-router'
 
 export default async function handleRequest(
   request: Request,
