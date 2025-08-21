2d9993ec27c8649045b4739414f6fed591608900
caprolactam <caprolactam113@gmail.com>
2025-08-21 21:20:28 +0900

    feat: vitest browserモード・ブラウザテストケースの追加

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,7 @@
 // cloudflare サービスをテスト環境で使用する設定
 // https://developers.cloudflare.com/workers/testing/vitest-integration/write-your-first-test/
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
@@ -7,7 +9,30 @@ export default defineConfig({
     coverage: {
       provider: 'istanbul',
     },
-    include: ['./app/**/*.test.ts'], // .tsxファイルを除外
+    projects: [
+      {
+        test: {
+          name: 'unit',
+          include: ['./app/**/*.test.{ts,tsx}'],
+          exclude: ['./app/**/*.browser.test.{ts,tsx}'],
+          environment: 'node',
+        },
+      },
+      {
+        plugins: [react(), tsconfigPaths()],
+        test: {
+          name: 'browser',
+          include: ['./app/**/*.browser.test.{ts,tsx}'],
+          browser: {
+            enabled: true,
+            provider: 'playwright',
+            instances: [
+              { browser: 'webkit' },
+            ],
+          },
+        },
+      },
+    ],
   },
 },
 )
